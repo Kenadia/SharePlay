bc54be5d4901f221255f536142061134bf2662a0
Ken and Wanchen <n/a>
2014-09-20 03:15:22 -0400

    It's done yo

--- crowdplay.js
+++ crowdplay.js
@@ -20,8 +20,65 @@ var handleSms = function (request, response) {
 
 if (Meteor.isClient) {
 
+  var currentSong;
+  var songQuery = Songs.find({});
+
   Template.main.queue = function () {
-    return Songs.find({});
+    return songQuery;
   };
 
+  songQuery.observeChanges({
+    added: function (id, song) {
+      if (currentSong != song.name) {
+        currentSong = song.name;
+        playTopSearchResult(currentSong);
+      }
+    }
+  });
+
+  Template.main.rendered = function () {
+    // Load IFrame Player API
+    var tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  };
+
+  // Initialize YouTube player
+  var player;
+  var playerReady = false;
+  onYouTubeIframeAPIReady = function () {
+    player = new YT.Player('youtube', {
+      height: '200',
+      width: '200',
+      videoId: '62E1MdaXcco',
+      events: {
+        // 'onReady': onPlayerReady,
+        // 'onStateChange': onPlayerStateChange
+      }
+    });
+  }
+  
+  var playTopSearchResult = function (keyword) {
+    console.log('play top ' + keyword);
+    var searchURI = 'http://gdata.youtube.com/feeds/api/videos?q=' +
+                    encodeURIComponent(keyword) +
+                    '&format=5&max-results=1&v=2&alt=jsonc';
+    $.ajax({
+      type: "GET",
+      url: searchURI,
+      dataType: "jsonp",
+      success: function(responseData, textStatus, XMLHttpRequest) {
+        if (responseData.data.items) {
+          var video = responseData.data.items[0];
+          player.loadVideoById(video.id, 0, 'small');
+        }
+      }
+    });
+  };
+
+}
+
+if (Meteor.isServer) {
+  Songs.remove({});
 }
