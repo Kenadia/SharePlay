33371e7d66257d38ca48ef4bb81df9e5c6ef2c3e
Ken Schiller <kenschiller@gmail.com>
2014-09-20 00:34:58 -0400

    Simplify

--- crowdplay.js
+++ crowdplay.js
@@ -10,17 +10,12 @@ Router.map(function () {
 });
 
 var handleSms = function (request, response) {
-  if (!request.body.Body) {
-    console.log("Empty request");
-    return;
-  } else {
-    console.log("Received an SMS with body:");
-    console.log(request.body.Body);
+  if (request.body.Body) {
     Songs.insert({name: request.body.Body});
+    var xml = '<?xml version="1.0" encoding="UTF-8"?><Response><Sms>Thanks for your song request!</Sms></Response>';
+    response.writeHead(200, {'Content-Type': 'text/xml'});
+    response.end(xml);
   }
-  var xml = '<?xml version="1.0" encoding="UTF-8"?><Response><Sms>Thanks for your song request!</Sms></Response>';
-  response.writeHead(200, {'Content-Type': 'text/xml'});
-  response.end(xml);
 };
 
 if (Meteor.isClient) {
@@ -28,29 +23,5 @@ if (Meteor.isClient) {
   Template.main.queue = function () {
     return Songs.find({});
   };
-
-}
-
-if (Meteor.isServer) {
-
-  // Constants and libraries
-  var accountSid = 'AC116fe83f10fb4d81941b83f71ca10f60'; 
-  var authToken = '745b0d3378f769e4918f0255cf61f850';
-
-  // Returns the result of an asynchronous call.
-  var asyncResult = function (asyncCall) {
-    var future = new Future();
-    asyncCall(Meteor.bindEnvironment(function (err, res) {
-      if (err) {
-        future.throw(err);
-      } else {
-        future.return(res)
-      }
-    }));
-    return future.wait();
-  };
-
-  Meteor.methods({
-  });
 
 }
