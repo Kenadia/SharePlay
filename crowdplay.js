ac8c9a23b69d9b0e80ec47e9efbc16a79ec4bab9
Ken, Chet, and Wanchen <n/a>
2014-09-20 00:15:38 -0400

    Basic twilio integration and markup

--- crowdplay.js
+++ crowdplay.js
@@ -1,23 +1,56 @@
+Songs = new Mongo.Collection("songs");
+
+Router.map(function () {
+  this.route('sms', {
+    where: 'server',
+    action: function () {
+      handleSms(this.request, this.response);
+    }
+  });
+});
+
+var handleSms = function (request, response) {
+  if (!request.body.Body) {
+    console.log("Empty request");
+    return;
+  } else {
+    console.log("Received an SMS with body:");
+    console.log(request.body.Body);
+    Songs.insert({name: request.body.Body});
+  }
+  var xml = '<?xml version="1.0" encoding="UTF-8"?><Response><Sms>Thanks for your song request!</Sms></Response>';
+  response.writeHead(200, {'Content-Type': 'text/xml'});
+  response.end(xml);
+};
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
+  Template.main.queue = function () {
+    return Songs.find({});
+  };
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+
+  // Constants and libraries
+  var accountSid = 'AC116fe83f10fb4d81941b83f71ca10f60'; 
+  var authToken = '745b0d3378f769e4918f0255cf61f850';
+
+  // Returns the result of an asynchronous call.
+  var asyncResult = function (asyncCall) {
+    var future = new Future();
+    asyncCall(Meteor.bindEnvironment(function (err, res) {
+      if (err) {
+        future.throw(err);
+      } else {
+        future.return(res)
+      }
+    }));
+    return future.wait();
+  };
+
+  Meteor.methods({
   });
+
 }
